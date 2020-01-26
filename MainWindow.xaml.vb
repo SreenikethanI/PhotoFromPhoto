88843be9e41e8293dfce40ec8793d9b59a62b934
Sreenikethan I <sreeni.s.iyer+github@gmail.com>
2020-01-26 11:23:31 +0400

    Commit 1

---
+++ MainWindow.xaml.vb
@@ -0,0 +1,15 @@
+﻿Public Class MainWindow
+    Public Sub MeLoad() Handles Me.Loaded
+
+    End Sub
+    Public Sub ThemeSelectionChanged()
+        If ThemeDark.IsChecked Then
+            Me.Style = Me.FindResource("Dark")
+        ElseIf ThemeLight.IsChecked Then
+            Me.Style = Nothing
+        Else
+            ThemeDark.IsChecked = True
+            ThemeSelectionChanged()
+        End If
+    End Sub
+End Class
